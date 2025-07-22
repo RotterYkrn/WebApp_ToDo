a9d07fa3d6063cd0e6fa04aff1e3eae3bdfdc5c5
RotterYkrn <sugaku.ohno@gmail.com>
2025-07-22 19:32:29 +0900

    vitest設定だけ用意

---
+++ stacks/ts-ts-prisma-mysql/frontend/vitest.config.ts
@@ -0,0 +1,13 @@
+// vitest.config.ts
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    include: ['tests/**/*.test.ts'],
+    // coverage: {            // カバレッジ設定 (オプション)
+    //   reporter: ['text', 'json', 'html'],
+    // },
+  },
+});
