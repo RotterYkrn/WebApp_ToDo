142fc86bc38dbea21c7b496ca0246769046c8b1d
RotterYkrn <sugaku.ohno@gmail.com>
2025-12-09 23:01:55 +0900

    shares/dist不要になった

--- stacks/react-nodejs/shared/dist/paths/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "./pages.js";
-export * from "./users.js";
-export * from "./auths.js";
-export * from "./tasks.js";
--- stacks/react-nodejs/shared/dist/schemas/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./auth.js";
-export * from "./settings.js";
-export { Task } from "./task.js";
-export * from "./user.js";
