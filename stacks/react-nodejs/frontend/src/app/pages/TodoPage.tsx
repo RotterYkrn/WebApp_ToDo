004c84b1956fc15177e8c7694406ef4287b58ae8
RotterYkrn <sugaku.ohno@gmail.com>
2026-01-02 16:44:47 +0900

    フォルダ名修正

--- stacks/react-nodejs/frontend/src/app/pages/TodoPage.tsx
+++ stacks/react-nodejs/frontend/src/app/pages/TodoPage.tsx
@@ -1,5 +1,5 @@
 import SignOutButton from "@/features/auths/components/SignOutButton";
-import TodoList from "@/features/todo/components/TodoList";
+import TodoList from "@/features/todos/components/TodoList";
 
 const TodoPage = () => {
     return (
--- stacks/react-nodejs/frontend/src/contexts/AppManager/AppLayer.ts
+++ stacks/react-nodejs/frontend/src/contexts/AppManager/AppLayer.ts
@@ -1,7 +1,7 @@
 import { AuthLive } from "@/features/auths";
 import { DailyPlanLive } from "@/features/daily-plans/services/DailyPlanLive";
 import { TaskLive } from "@/features/tasks/services/TaskLive";
-import { TodoLive } from "@/features/todo/services/TodoLive";
+import { TodoLive } from "@/features/todos/services/TodoLive";
 import { ApiLive } from "@/shared/http";
 import { ConsoleLoggerLive } from "@/shared/logger";
 import { UILive } from "@/shared/ui";
